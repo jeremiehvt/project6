85b6408a0772bd174bd2e674400962dd2e416f24
jeremiehvt <jeremiehvt@gmail.com>
2018-05-18 14:59:04 +0200

    modify footer an add ogout link

--- wp-content/themes/axcesstheme/footer.php
+++ wp-content/themes/axcesstheme/footer.php
@@ -3,9 +3,20 @@
 <div class="jumbotron jumbotron-fluid">
 	<div class="container">
 		<ul class="list-unstyled">
-			<li><a href="<?php echo wp_login_url(home_url())?>">administration</a></li>
+			<?php 
+				if (is_user_logged_in()) { ?>
+
+					<li><a href="<?php echo wp_logout_url(home_url())?>">se déconnecter</a></li>
+					
+				<?php } else { ?>
+
+					<li><a href="<?php echo wp_login_url(home_url())?>">administration</a></li>
+
+				<?php }
+			 ?>
+			
 		</ul>
-		<p>site réalisé par ©jeremiehvart | <a href="mailto:jeremiehvt@gmail.com">jeremiehvt@gmail.com</a></p>
+		<p>site réalisé par ©jeremiehavart | <a href="mailto:jeremiehvt@gmail.com">jeremiehvt@gmail.com</a></p>
 	</div>
 	
 </div>
